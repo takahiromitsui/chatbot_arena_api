94f1efd26e6b69c584e1a8d20b88b43505ad0f29
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-05-22 09:55:57 +0200

    feat: create ResponseGenerator

---
+++ src/response_generator.py
@@ -0,0 +1,54 @@
+import asyncio
+from dataclasses import dataclass
+from flask import json
+
+# local
+from src.azure_factory import AzureOpenAIFactory
+
+
+@dataclass
+class ModelResponse:
+    blind_name: str  # A or B
+    display_name: str  # e.g., gpt3
+    full_name: str  # e.g., sometech-gpt-35-turbo
+    response: str  # answer from a model
+
+
+class ResponseGenerator:
+    def __init__(self, factory: AzureOpenAIFactory):
+        self.factory = factory
+
+    async def generate_response(self, prompt: str) -> list[ModelResponse]:
+        client = self.factory.create()
+        models = self.factory.pick_two_random_models()
+        responses = []
+        for model in models:
+            completion = await client.chat.completions.create(
+                model=model.full_name,
+                messages=[
+                    {
+                        "role": "user",
+                        "content": prompt,
+                    },
+                ],
+            )
+            res = json.loads(completion.model_dump_json(indent=2))
+            message_content = res["choices"][0]["message"]["content"]
+            data = {
+                "blind_name": model.blind_name,
+                "display_name": model.display_name,
+                "full_name": model.full_name,
+                "response": message_content,
+            }
+            responses.append(data)
+        return responses
+
+
+if __name__ == "__main__":
+    # poetry run python -m src.response_generator
+    from src.config import settings
+
+    factory = AzureOpenAIFactory(settings)
+    generator = ResponseGenerator(factory)
+    responses = asyncio.run(generator.generate_response("What is the meaning of life?"))
+    print(responses)
